62b3e09964ab20a3ffa0beb70412c842d2643c4b
abhayk424 <abhay424@gmail.com>
2024-08-03 18:41:06 +0530

    Fix flake8 style issues - fixed num characters in a line

--- model.py
+++ model.py
@@ -9,7 +9,8 @@ boston = load_boston()
 X, y = boston.data, boston.target
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2,
+                                                     random_state=42 )
 
 # Train model
 model = LinearRegression()
