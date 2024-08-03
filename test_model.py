92b9956390b75c213ca3fed06102cd2d467c893d
abhayk424 <abhay424@gmail.com>
2024-08-03 18:44:15 +0530

    Fix flake8 style issues - removed extra whitespace before ) and after (

--- test_model.py
+++ test_model.py
@@ -12,8 +12,8 @@ def test_model_training():
     X, y = boston.data, boston.target
 
     # Split data
-    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2,
-                                                         random_state=42 )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                        random_state=42)
 
     # Train model
     model = LinearRegression()
